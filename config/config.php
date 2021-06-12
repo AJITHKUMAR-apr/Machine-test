7eafc11da1e6ac40d83c96059da7d16bac7952d5
AJITHKUMAR-apr <85723550+AJITHKUMAR-apr@users.noreply.github.com>
2021-06-12 18:46:50 +0530

    created config file

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+// Database
+defined('DB_HOST') ? NULL : define('DB_HOST', 'localhost');
+defined('DB_USER') ? NULL : define('DB_USER', 'testuser');
+defined('DB_PASSWORD') ? NULL : define('DB_PASSWORD', 'machinetest');
+defined('DB_NAME') ? NULL : define('DB_NAME', 'test');
+
+defined('PAGE_PATH_NAME') ? NULL : define('PAGE_PATH_NAME', "http://localhost/stocks/"); 
+?>
